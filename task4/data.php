8484e330a5b214139645c9cfd749b202ea8fe163
yosef <yoseffmohamedd01@gmail.com>
2023-06-20 13:33:26 +0300

    all tasks

---
+++ task4/data.php
@@ -0,0 +1,27 @@
+<?php
+
+$products = [
+
+    'product1' => ['name' => 'aa', 'price' => '456', 'clr' => 'red'],
+    'product2' => ['name' => 'bb', 'price' => '233', 'clr' => 'blue'],
+    'product3' => ['name' => 'cc', 'price' => '432', 'clr' => 'yellow'],
+    'product4' => ['name' => 'dd', 'price' => '567', 'clr' => 'greeen'],
+    'product5' => ['name' => 'ss', 'price' => '436', 'clr' => 'black'],
+    'product6' => ['name' => 'ee', 'price' => '434', 'clr' => 'orange'],
+];
+
+
+
+$users = [
+'user1' => ['username' => 'youssefsenior', 'password' => '12345678','yoseffmohamed001@gmail.com'],
+'user2' => ['username' => 'youssef', 'password' => '12345678','yoseffmohamed001@gmail.com'],
+'user3' => ['username' => 'youssef', 'password' => '12345678','yoseffmohamed001@gmail.com'],
+'user4' => ['username' => 'youssef', 'password' => '12345678','yoseffmohamed001@gmail.com'],
+'user5' => ['username' => 'youssef', 'password' => '12345678','yoseffmohamed001@gmail.com'],
+'user6' => ['username' => 'youssef', 'password' => '12345678','yoseffmohamed001@gmail.com'],
+
+];
+
+
+
+?>
---
+++ task4/login.php
@@ -0,0 +1,80 @@
+ <?php
+  require_once 'data.php';
+
+  $errors =[];  
+if(isset($_POST['username1'])){
+  $username = $_POST['username1'];
+  $passwrod = $_POST['password1'];
+  $email = $_POST['email1'];
+  $flag = 0;
+  if(! empty($username)){
+    if(strlen($username) > 8){
+     if(preg_match('/senior$/',$username)){
+      $flag++;
+     } else {
+      $errors['usernameRegx'] = 'plz end text with senior';
+     }
+     } else{
+      $errors['userLenght'] = ">8";
+     }
+    } else{
+      $errors['username empty'] = 'plz write username';
+    }
+    if($flag == 1){
+      foreach($users as $user){
+        if($user['username'] == $username && $user['password'] == $passwrod){
+          header('location:index.php');
+        } 
+      }
+    }
+    
+    
+    
+      var_dump(($_GET));
+      var_dump(($_POST));
+  }
+
+
+
+  ?>
+  
+  <!doctype html>
+
+<html lang="en"> 
+
+ <head> 
+
+  <meta charset="UTF-8"> 
+
+  <title>CodePen - Animated Login Form using Html &amp; CSS Only</title> 
+
+  <link rel="stylesheet" href="./style.css"> 
+  
+
+ </head> 
+
+ <body> <!-- partial:index.partial.html --> 
+
+  <section> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> 
+
+   <div class="signin"> 
+
+   <form action="" method="post">
+    <input type="text" name="username1">
+    <input type="text" name="email1">
+    <input type="text" name="password1">
+    <button>login</button>
+   <?php foreach($errors as $error): ?>
+    <?=  $error; ?>
+    <?php endforeach ?>
+   </form>
+
+   </div> 
+
+  </section> <!-- partial --> 
+
+ </body>
+
+</html>
+
+ 
