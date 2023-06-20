e137593c0a1262ebcd04293c21715326d3c1aa28
yosef <yoseffmohamedd01@gmail.com>
2023-06-21 00:58:56 +0300

    task 4

--- task4/data.php
+++ task4/data.php
@@ -6,7 +6,7 @@ $products = [
     'product2' => ['name' => 'bb', 'price' => '233', 'clr' => 'blue','src' => 'https://images.unsplash.com/photo-1687190380431-c7cef1378247?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60'],
     'product3' => ['name' => 'cc', 'price' => '432', 'clr' => 'yellow','src' => 'https://images.unsplash.com/photo-1687193913636-f48ac75b089b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60'],
     'product4' => ['name' => 'dd', 'price' => '567', 'clr' => 'greeen','src' => 'https://images.unsplash.com/photo-1687191886930-a246ada3cea2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60'],
-    'product5' => ['name' => 'ss', 'price' => '436', 'clr' => 'black',,'src' => 'https://images.unsplash.com/photo-1686893287975-5ca0c6b18bf9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60'],
+    'product5' => ['name' => 'ss', 'price' => '436', 'clr' => 'black','src' => 'https://images.unsplash.com/photo-1686893287975-5ca0c6b18bf9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60'],
     'product6' => ['name' => 'ee', 'price' => '434', 'clr' => 'orange','src' => 'https://images.unsplash.com/photo-1666919643134-d97687c1826c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60'],
 ];
 
--- task4/index.php
+++ task4/index.php
@@ -92,7 +92,7 @@
       <?php foreach($products as $product) : ?>
 
         <div class="col" style = 'background-color: <?= $product['clr'] ?>;'>
-
+        <img src="<?= $product['src'] ?>">
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">صورة مصغرة</text></svg>
             <div class="card-body">
@@ -114,7 +114,7 @@
   </div>
 </main>
 
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
         <div class="col">
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
